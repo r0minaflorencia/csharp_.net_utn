dd80eafefbe4239c352ab1837b4c38e8c364e07b
Romina Avalos <rflorencia.avalos@gmail.com>
2024-08-28 17:57:47 -0300

    first commit

---
+++ Ejercicio21/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Diseña un programa que, dado un número real que debe representar la calificación numérica de un
+examen, proporcione la calificación cualitativa correspondiente al número dado. La calificación
+cualitativa será una de las siguientes:
+✭✭Suspenso✮✮ (nota menor que 5),
+✭✭Aprobado✮✮ (nota mayor o igual que 5, pero menor que 7),
+✭✭Notable✮✮ (nota mayor o igual que 7, pero menor que 8.5),
+✭✭Sobresaliente✮✮ (nota mayor o igual que 8.5, pero menor que 10),
+✭✭Matrícula de Honor✮✮ (nota 10). */
+
+Console.WriteLine("Ingresa la calificación:");
+
+float calificacion = float.Parse(Console.ReadLine());
+
+if (calificacion < 5)
+{
+    Console.WriteLine($"Tu calificación es: {calificacion}. Suspenso");
+}
+else if (calificacion >= 5 && calificacion < 7)
+{
+    Console.WriteLine($"Tu calificación es: {calificacion}. Aprobado");
+}
+else if (calificacion >= 7 && calificacion < 8.5)
+{
+    Console.WriteLine($"Tu calificación es: {calificacion}. Notable");
+}
+else if (calificacion >= 8.5 && calificacion < 10)
+{
+    Console.WriteLine($"Tu calificación es: {calificacion}. Sobresaliente");
+} else if (calificacion == 10) {
+     Console.WriteLine($"Tu calificación es: {calificacion}. Matricula de Honor");
+}
+else
+{
+    Console.WriteLine("Debes ingresar una nota entre 0 y 10.");
+}
+
+
+
---
+++ Ejercicio22/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Realizar un programa que calcule nota media de 3 asignaturas de cada uno de los 5 alumnos de la clase. */
+
+Console.WriteLine("\t Calcular promedio");
+
+float promedio, suma;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("\n Alumno n° " + (i + 1));
+
+    suma = 0;
+
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.Write($"Ingresa la nota de la {j}° asignatura: ");
+        float nota = float.Parse(Console.ReadLine());
+        suma += nota;
+    }
+
+    promedio = suma / 3;
+    Console.Write($"\t El promedio es {Math.Round(promedio)}\n"); //arreglar
+
+}
+
