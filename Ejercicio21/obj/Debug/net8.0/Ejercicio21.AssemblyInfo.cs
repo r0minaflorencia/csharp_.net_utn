2e14472f8dc05711c34406d569c06be09c9cf5d8
Romina Avalos <rflorencia.avalos@gmail.com>
2024-08-28 22:25:38 -0300

    updates

--- Ejercicio21/obj/Debug/net8.0/Ejercicio21.AssemblyInfo.cs
+++ Ejercicio21/obj/Debug/net8.0/Ejercicio21.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd80eafefbe4239c352ab1837b4c38e8c364e07b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe8268105a1e59f353e062bada3cf1d7acef5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Ejercicio23/Program.cs
@@ -0,0 +1,69 @@
+﻿/* Algoritmo simplificado (20 líneas de código) de un Cajero automático Este ejercicio podría ser muy
+largo o complicarse más, pero lo que importa en una estructuración inicial simple para poder
+ampliarlo, si se desea, más adelante. Se pueden obtener tantas soluciones como personas
+resuelvan este ejercicio. */
+
+Console.WriteLine("\t Cajero automático");
+
+float saldo = 1000.0f;
+int opcion;
+bool salir = false;
+
+do
+{
+    Console.WriteLine("\n 1- Consultar saldo\n 2- Extraer efectivo\n 3- Depositar\n 4- Salir\n");
+    opcion = int.Parse(Console.ReadLine());
+
+
+    switch (opcion)
+    {
+        case 1:
+            MostrarSaldo();
+            break;
+        case 2:
+            saldo = ExtraerSaldo();
+            break;
+        case 3:
+            Depositar();
+            break;
+        case 4:
+            Console.WriteLine("Log out.");
+            salir = true;
+            break;
+        default:
+            break;
+    }
+
+} while (!salir);
+
+static void Depositar()
+{
+    Console.WriteLine("En construcción...");
+    //TODO here
+}
+
+float ExtraerSaldo()
+{
+    Console.Write("Ingrese el monto a retirar: ");
+    float monto = float.Parse(Console.ReadLine());
+
+    if (monto <= saldo)
+    {
+        saldo -= monto;
+        Console.WriteLine($"Retiro exitoso");
+    }
+    else
+    {
+        Console.WriteLine("Fondos insuficientes.");
+    }
+
+    return saldo;
+}
+
+void MostrarSaldo()
+{
+    Console.WriteLine($"Saldo: $ {saldo}");
+}
+
+
+
